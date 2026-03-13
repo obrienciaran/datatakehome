1492c5a7219b09730296a36dd2a2789b1d17b091
Ciaran <31419980+obrienciaran@users.noreply.github.com>
2026-03-13 18:57:25 +0100

    Update frequent_attenders.sql

--- dbt/models/marts/frequent_attenders.sql
+++ dbt/models/marts/frequent_attenders.sql
@@ -17,6 +17,24 @@ patients as (
     where _dedup_rn = 1
 ),
 
+-- This approach is O(n log n) so a bit better. It uses a sliding window and counts incrementally rather than comparing every pair from table A and B
+--    encounter_with_rolling as (
+--    select
+--        patient_id,
+--        encounter_id,
+--        encounter_class,
+--        encounter_start,
+--        count(*) over (
+--            partition by patient_id
+--            order by unix_seconds(encounter_start)
+--            -- 31536000 is the number of seconds in 365 days
+--            -- will drift due to leap years, but so will the below approach
+--            range between 31536000 preceding and current row
+--        ) as visits_rolling_12m
+--    from encounters
+--)
+
+-- This is a self join which can get computationally expensive as it is O(n²). See the above comment for a better way.
 encounter_with_rolling as (
     select
         e1.patient_id,
